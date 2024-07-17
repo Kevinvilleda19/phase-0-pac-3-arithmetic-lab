44965c775693ce625a52c80e97dc9b95f78f007a
Kevin <kvilleda10566@gmail.com>
2024-07-17 17:29:30 -0400

    Done

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+return a + b
+}
+function subtract(a , b) {
+    return a - b
+}
+function multiply(num1 , num2) {
+    return num1 * num2
+}
+function divide(a, b) {
+    return a / b
+}
+let number = n;
+
+function increment(number) {
+  return (number += 1);
+}
+
+function decrement(number) {
+  return (number -= 1);
+}
+function makeInt(number){
+    return(parseInt(number,10));
+    
+}
+function preserveDecimal(number){
+    return(parseFloat(number));
+    
+}
+
